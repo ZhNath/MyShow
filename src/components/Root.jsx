99b65d880d1510236ecd1ec4d5d4c85338ddebba
Nath-DCI <natalia.zharova@dci-student.org>
2024-05-25 05:13:14 +0200

    color mode

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -3,8 +3,16 @@ import { Outlet } from "react-router-dom";
 import { AuthProvider } from "../globalContext/AuthContext";
 import Footer from "./Footer/Footer";
 import { FilterBy } from "./Main/FilterBy";
+import { useThemeContext } from "../globalContext/ThemeContext";
+import { useEffect } from "react";
 
 function Root() {
+  const { theme } = useThemeContext();
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
     <>
       <AuthProvider>
---
+++ src/globalContext/ThemeContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useThemeContext = () => {
+  return useContext(ThemeContext);
+};
